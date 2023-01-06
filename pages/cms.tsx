def4b5b8369e2f343847c16204a340db521385d0
Avramu98 <vlad_avram98@yahoo.com>
2023-01-06 18:56:31 +0200

    test build

--- pages/cms.tsx
+++ pages/cms.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Button, Container, Divider } from '@mui/material';
 
+
 import axios from 'axios';
 import { AppHeader } from '@/components/elements/cms';
 import { ModalTypeI, PictureI } from '../types';
@@ -9,24 +10,33 @@ import 'swiper/css/effect-cards';
 import SwiperContainer from '@/components/elements/swiper-container';
 import useModal from '@/lib/hooks/useModal';
 import CreateModal from '@/components/elements/cms/CreateModal';
+import useSWR from 'swr';
 
 export async function getServerSideProps(ctx: { res: { getHeader: (arg0: string) => any; }; }) {
-  const pictures = await axios.get('https://rasno-ph-phtj.vercel.app//api/pictures');
-  const data = pictures.data;
+  // const pictures = await axios.get('/api/pictures');
+  // const data = pictures.data;
   const os = ctx.res.getHeader('x-os-type');
   return {
-    props: { picturesData: data.picturesData, os },
+    props: { os },
   };
 }
+
+const fetcher = async () => {
+  const data = await axios.get('./api/pictures');
+  return {
+    picturesData: data.data.picturesData,
+  };
+};
 
 export interface CmsInterface {
   picturesData: PictureI[]
 }
 
 // @ts-ignore
-const Cms = ({ picturesData, os }) => {
-  const events = picturesData.filter((picture: PictureI) => picture.category === 'EVENT');
-  const landscape = picturesData.filter((picture: PictureI) => picture.category === 'LANDSCAPE');
+const Cms = ({ os }) => {
+  const { data } = useSWR('photos', fetcher);
+  const events = data?.picturesData.filter((picture: PictureI) => picture.category === 'EVENT');
+  const landscape = data?.picturesData.filter((picture: PictureI) => picture.category === 'LANDSCAPE');
   const [toggleMode, setToggle] = useState<boolean>(false);
   const { open, toggle, modalType } = useModal();
 
