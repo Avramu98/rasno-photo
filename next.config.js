02867826a99a13a27b321b64fb2286969f5b8b03
Avramu98 <vlad_avram98@yahoo.com>
2023-01-07 17:46:36 +0200

    tesT

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-      domains: ['rasno-ph.s3.eu-west-3.amazonaws.com']
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ['rasno-ph.s3.eu-west-3.amazonaws.com']
+    },
     compiler: {
-    styledComponents: {
-      ssr: true
-  }
+        styledComponents: {
+            ssr: true
+        }
+    },
+    experimental: {
+        optimizeCss: true
     }
-
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({})
 
 module.exports = nextConfig
